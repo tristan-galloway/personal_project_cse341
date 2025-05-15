f9ab891298340b250bc5e4e0b4a08e1e9eb8a1b8
Tristan Galloway <tristan.a.galloway@gmail.com>
2025-05-15 13:42:43 -0600

    Removing host key to make it dynamic

--- swagger.js
+++ swagger.js
@@ -1,16 +1,10 @@
 const swaggerAutogen = require('swagger-autogen')();
 
-const host =
-  process.env.NODE_ENV === 'production'
-    ? 'https://cse340-spring25.onrender.com'
-    : 'localhost:8080';
-
 const doc = {
   info: {
     title: 'Contacts API',
     description: 'API for managing contacts with CRUD operations.'
-  },
-  host: host
+  }
 };
 
 const outputFile = './swagger-output.json';
